6731cce7f27c504e1325d354251cbe39aad6a463
Kishanthakur <kishanthakur818@gmail.com>
2023-08-10 00:17:19 +0530

    added elements in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,16 @@ export default function Navbar() {
           <Image
             alt="Doof meat"
             src="/doof-meat-logo.svg"
+            className="hidden xl:block"
             width={300}
             height={30}
+          />
+          <Image
+            alt="Doof meat"
+            src="/logo-2.png"
+            className="xl:hidden"
+            width={70}
+            height={20}
           />
         </div>
         <div className="flex">
@@ -20,15 +28,46 @@ export default function Navbar() {
             className="lg:w-96 ml-5 sm:ml-0 px-1 py-1 sm:py-2 rounded border-2 border-gray-300"
           />
         </div>
-        <div className="flex flex-row justify-center items-center">
-          <button className="ml-6 sm:ml-0 mr-4 xl:mr-6 px-2 sm:px-3 py-1 sm:py-2 rounded text-white font-semibold bg-red-500">
-            Login
+        <div className="flex flex-row justify-center items-center ">
+          <button className="hidden sm:flex  flex-row justify-center items-center ml-6 sm:ml-0 mr-4 xl:mr-2 px-2 sm:px-2 py-1 sm:py-2 rounded text-black font-semibold ">
+            <Image
+              alt="Image"
+              src="/about-icon.png"
+              width={20}
+              height={10}
+              className="mr-2"
+            />
+            About
+          </button>
+          <button className="hidden sm:flex flex-row justify-center items-center ml-6 sm:ml-0 mr-4 xl:mr-2 px-2 sm:px-2 py-1 sm:py-2 rounded text-black font-semibold ">
+            <Image
+              alt="Image"
+              src="/contact-icon.png"
+              width={20}
+              height={10}
+              className="mr-2 align-middle"
+            />
+            Contact
+          </button>
+          <button className="hidden sm:flex flex-row justify-center items-center ml-6 sm:ml-0 mr-4 xl:mr-2 px-2 sm:px-2 py-1 sm:py-2 rounded text-black font-semibold ">
+            <Image
+              alt="Image"
+              src="/shop-icon.png"
+              width={20}
+              height={10}
+              className="mr-2"
+            />
+            Shop
+          </button>
+
+          <button className="ml-6 sm:ml-0 mr-2 xl:mr-6 px-2 sm:px-3 py-1 sm:py-2 rounded text-white font-semibold bg-red-500">
+            Account
           </button>
           <Image
-            className="mr-3"
+            className="mr-4"
             alt="Bag"
             src="/bag-icon.png"
-            width={40}
+            width={38}
             height={10}
           />
         </div>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -80,7 +80,7 @@ function ProductCard() {
         </div>
         <a
           href="#"
-          className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          className="flex items-center justify-center rounded-md bg-red-500 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-red-400"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -96,7 +96,7 @@ function ProductCard() {
               d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
             />
           </svg>
-          Add to cart
+          ADD TO CART
         </a>
       </div>
     </div>
